6e99cc2d2399428b81c5fa98a7911aa6bc6befc2
JGOPN <weter506@hotmail.com>
2022-10-06 22:51:24 +0100

    First commit

---
+++ script/script.js
@@ -0,0 +1,18 @@
+const divs=["classificacoes","ajuda"];
+
+function closeAllDivs(){
+    //esconde todas as divs floating abertas
+    divs.forEach(str => document.getElementById(str).style.display = "none" );
+}
+
+function toggleDiv(x){
+    //esconde ou mostra div, recebe string id
+    //da primeira vez js nao reconhece display none, precisa clicar 2 vezes
+    //Solucao=inicializar classes com none neste arquivo?
+    if(document.getElementById(x).style.display !== "none")
+        document.getElementById(x).style.display = "none";
+    else{
+        closeAllDivs();
+        document.getElementById(x).style.display = "block";
+    }
+}
